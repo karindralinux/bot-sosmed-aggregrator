56f1ecda5bec49dec458250464b1ed8721c198b6
Arjun Nafi <arjunnafi04@gmail.com>
2024-05-29 21:19:57 +0700

    changes command hears to action for handle choosen things

--- services/things.ts
+++ services/things.ts
@@ -3,32 +3,35 @@ import { Database } from "bun:sqlite";
 import { getStateUser, setThingTypeUser } from "../helpers/cache";
 
 export function register(bot: Telegraf, cache: Database) {
-    bot.hears(["KTP", "SIM", "STNK"], async (ctx: Context) => {
-
-        const message: any = ctx.message;
-        const thingType = message?.text || '';
-        const userId = message?.from.id.toString() || '';
-
-        const { state } = getStateUser(cache, userId);
-
-        if (state == 0) {
-
-            setThingTypeUser(cache, userId, thingType)
-
-            await ctx.reply(`Baik, kamu cari ${thingType} ya. _Menyiapkan template..._`, {
-                reply_markup: {
-                    remove_keyboard: true,
-                },
-                parse_mode: 'Markdown'
-            })
-
-            await ctx.telegram.sendMessage(userId, `
-                *Nama :* \n*Lokasi Terakhir :* \n*Rentang Waktu Hilang :* \n
-            `, {
-                parse_mode: 'Markdown'
-            });
-        }
+    bot.action(["KTP", "SIM", "STNK"], async (ctx: Context) => {
+        await handleThing(ctx, cache);
     })
 
     return []
+}
+
+async function handleThing(ctx: Context, cache: Database) {
+    const message: any = ctx.message;
+    const thingType = message?.text || '';
+    const userId = ctx?.from?.id.toString() || '';
+
+    const { state } = getStateUser(cache, userId);
+
+    if (state == 0) {
+
+        setThingTypeUser(cache, userId, thingType)
+
+        await ctx.reply(`Baik, kamu cari ${thingType} ya. _Menyiapkan template..._`, {
+            reply_markup: {
+                remove_keyboard: true,
+            },
+            parse_mode: 'Markdown'
+        })
+
+        await ctx.telegram.sendMessage(userId, `
+            *Nama :* \n*Lokasi Terakhir :* \n*Rentang Waktu Hilang :* \n
+        `, {
+            parse_mode: 'Markdown'
+        });
+    }
 }
